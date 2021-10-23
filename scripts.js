7e4c15aed7c191911eeac7101e7a300ae5560bdd
Clifton McCook <cliftonmccook@gmail.com>
2021-10-22 23:02:06 -0500

    Edited scripts.js

--- scripts.js
+++ scripts.js
@@ -6,10 +6,9 @@ window.onload = () => {
         event.preventDefault();
         let email = document.querySelector('input').value;
         let msg = document.getElementsByClassName("message")[0];
-        let pattern = /\w+@\w+.+\w/g;
         if (email == ""){
-            msg.innerHTML = "Please enter a valid email address."
-        } else if (email.match(pattern)) {
+            msg.innerHTML = "Please enter a valid email address.";
+        } else {
             msg.innerHTML = `Thank you! Your email address ${email} has been added to our mailing list!`;
         }
     } );
